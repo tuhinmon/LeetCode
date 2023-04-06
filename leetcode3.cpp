24d1fb03c3247e360475d63b6bff7d21ae11a3ca
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-06 09:55:47 +0530

    okkk

---
+++ leetcode3.cpp
@@ -0,0 +1,9 @@
+// Kth Smallest Element in a Sorted Matrix
+vector<int>v;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                v.push_back(matrix[i][j]);
+            }
+        }
+        sort(v.begin(),v.end());
+        return v[k-1];
