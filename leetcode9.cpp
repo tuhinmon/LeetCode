942c8b3eebfd9ae19cd41ede2b607e2c2938630c
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-15 08:58:35 +0530

    Binary Tree Inorder Traversal

---
+++ leetcode9.cpp
@@ -0,0 +1,16 @@
+//Binary Tree Inorder Traversal
+void inorder(TreeNode* root,vector<int>&ans){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left,ans);
+    ans.push_back(root->val);
+    inorder(root->right,ans);
+}
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        inorder(root,ans);
+        return ans;
+
+        
+    }
