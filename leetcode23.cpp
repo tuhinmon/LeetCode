3f25a598265bfe5574ec83eb08d27f4fe674e413
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-20 13:13:10 +0530

    Permutations

---
+++ leetcode23.cpp
@@ -0,0 +1,22 @@
+//Permutations
+void solve(vector<int>& nums,vector<vector<int>>&ans,int i){
+    // base case 
+    if(i>=nums.size()){
+        ans.push_back(nums);
+        return ;
+    }
+    for(int j=i;j<nums.size();j++){
+        swap(nums[i],nums[j]);
+        solve(nums,ans,i+1);
+        // backtracking
+         swap(nums[i],nums[j]);
+
+    }
+}
+public:
+ vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int i=0;
+        solve(nums,ans,i);
+        return ans;
+        
