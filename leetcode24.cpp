f2ee92011f6299b8b2e11652a5c4ace697a9ffde
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-20 13:56:52 +0530

    Group Anagrams

---
+++ leetcode24.cpp
@@ -0,0 +1,18 @@
+//Group Anagrams
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        vector<string>temp;
+        unordered_map<string,vector<string>>mpp;
+        for(auto x:strs){
+            string temp = x;
+            sort(x.begin(),x.end());
+            mpp[x].push_back(temp);
+        }
+        for(auto x:mpp){
+            vector<string>temp = x.second;
+            ans.push_back(temp);
+        }
+        return ans;
+
+        
+    }
