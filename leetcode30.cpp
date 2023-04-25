cfd0177632d91daddeadb4220035b7259211cd5c
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-25 11:10:42 +0530

    Kth Largest Element in an Array

---
+++ leetcode30.cpp
@@ -0,0 +1,13 @@
+//Kth Largest Element in an Array
+ int findKthLargest(vector<int>& nums, int k) {
+        int size =0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            size++;
+
+        }
+        int largest = nums[size-k];
+        return largest;
+
+
+    }
