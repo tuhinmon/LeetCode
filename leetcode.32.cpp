fff23f6d6ff93474dea088422d83aa7295d905d4
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-04-27 10:05:55 +0530

    Sort Colors

---
+++ leetcode.32.cpp
@@ -0,0 +1,33 @@
+//Sort Colors
+void sortColors(vector<int>& nums) {
+        int count0=0;
+        int count1 =0;
+        int count2=0;
+        for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+            count0++;
+        }
+        else if(nums[i]==1){
+            count1++;
+        }
+        else{
+            count2++;
+        }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(count0-->0){
+                nums[i]=0;
+            }
+            else if(count1-->0){
+                nums[i]=1;
+            }
+            else{
+                nums[i]=2;
+            }
+        }
+
+
+        
+  
+        
+    }
