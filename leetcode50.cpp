cf40797e01d26050939456e5bbdaa1ccc0567c94
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-07-03 13:16:10 +0530

    Rotate Array

---
+++ leetcode50.cpp
@@ -0,0 +1,16 @@
+//Rotate Array
+void reverse(vector<int>& nums,int low,int high){
+    while(low<=high){
+        swap(nums[low],nums[high]);
+        low++;
+        high--;
+    }
+}
+    void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums,0,nums.size()-k-1);
+        reverse(nums,nums.size()-k,nums.size()-1);
+        reverse(nums,0,nums.size()-1);
+
+        
+    }
