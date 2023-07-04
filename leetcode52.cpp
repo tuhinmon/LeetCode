d11aca3784a0fa1f44da1971e05b96cc65c2884f
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-07-04 12:41:24 +0530

    Product of Array Except Self

---
+++ leetcode52.cpp
@@ -0,0 +1,17 @@
+//Product of Array Except Self
+vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>ans(nums.size());
+        int x=1;
+        for(int i=0;i<nums.size();i++){
+             x=x*nums[i];
+             ans[i]=x;
+             }
+             x=1;
+             for(int i = nums.size()-1;i>0;i--){
+                 ans[i]=x*ans[i-1];
+                 x=x*nums[i];
+             }
+             ans[0]=x;
+             return ans;
+        
+    }
