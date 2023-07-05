a5efaa35fc114bb5347b06846fcd016638b2b4c7
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-07-05 09:37:43 +0530

    Container With Most Water

--- leetcode53.cpp
+++ leetcode53.cpp
@@ -1,1 +1,23 @@
 //Container With Most Water
+int maxArea(vector<int>& height) {
+        // T.C = 0(N);
+        // S.C = 0(1);
+       int i=0;
+       int j = height.size()-1;
+       int ans =0;
+       while(i<j){
+           int k = min(height[i],height[j]);
+           int area = (j-i)*k;
+             ans = max(ans,area);
+           if(height[i]<height[j]){
+               i++;
+           }
+           else{
+               j--;
+           }
+
+       }
+       return ans;
+
+        
+    }
