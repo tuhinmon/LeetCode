43f27d442df08aad7ab29b4976e0f4032bb2c1ab
Tuhin Mondal <tuhinmondal746@gmail.com>
2023-07-09 09:40:40 +0530

    Rotate Image

---
+++ leetcode54.cpp
@@ -0,0 +1,14 @@
+// Rotate Image
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        // every colums change in row
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        // reverse every row
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
